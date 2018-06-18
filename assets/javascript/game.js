f0254a11e6171c2ceae67b78013f2ee03e6de2cf
KamiKozy <kamikozytpp@gmail.com>
2018-06-17 21:46:44 -0400

    noticed error that caused variables to not load (fixed) but broke compSelect letter resetting on win/loss

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,55 @@
 var wins = 0;
 var losses = 0;
 var x = 10;
-//set = x, this allows me to change guesses allowed easily and it work throughout code
+//made variable x, this allows me to change guesses allowed easily and it work throughout code
 var guessesRemaining = x;
 var guessedSoFar = ""
 
 var compChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-var c = Math.floor(Math.random() * 25);
-var compSelect = compChoice[c];
+
+function clear(compSelect) { //allows function to be called to reset after a win/loss and reset guesses, generate new letter
+    var c = Math.floor(Math.random() * 25);
+    var compSelect = compChoice[c];
+    guessedSoFar = "";
+    guessesRemaining = x;
+    console.log(c);
+    console.log(compSelect);
+    return compSelect; // spits back letter chosen to outside of function
+};
+
+var compSelect = clear(compSelect) //issue with this variable being always the first when page loads. Working on fixing
 
 document.onkeyup = function (event) {
     var userGuess = event.key;
 
-    if (compChoice.indexOf(userGuess) !== -1) {
-        if (userGuess === compSelect) {
-            wins++;
-            alert("You were correct! The letter was " + compSelect);
-            guessedSoFar = "";
-            guessesRemaining = x;
+    //used this area to check if userGuess has already been tried to prevent duplicating
+    var isDuplicate = false; //originally makes the duplicate false, so that it must be changed to true
+    for (var i = 0; i < guessedSoFar.length; i++) {
+        if (userGuess == guessedSoFar[i]) {
+            isDuplicate = true; //if userGuess is in array of guessedSoFar it turns true, alerts, and stops 
+            alert("You already tried that letter!");
         }
-        else {
-            guessesRemaining--;
-            guessedSoFar = userGuess + " " + guessedSoFar;
+    };
+    if (!isDuplicate) { //verifies that isDuplicate is still false. Without this, counters still move
+        if (compChoice.indexOf(userGuess) !== -1) { //verifies user selects a possible option
+            if (userGuess === compSelect) { //if user guesses correct letter
+                wins++;
+                alert("You were correct! The letter was " + compSelect);
+                clear();
+            }
+            else { //if user does not select correct letter
+                guessesRemaining--;
+                guessedSoFar = userGuess + " " + guessedSoFar;
 
-        }
-        if (guessesRemaining === 0) {
-            losses++;    
-            alert("Ouch! Out of guesses!");
-            guessedSoFar = "";
-            guessesRemaining = x;
-        }
+            }
+            if (guessesRemaining === 0) { //if user runs out of guesses
+                losses++;
+                alert("Ouch! Out of guesses!");
+                clear();
+            };
 
-
+        };
     };
     document.getElementById("wins").innerText = wins;
     document.getElementById("losses").innerText = losses;
